0a8a3c4c63ff8456ced2be0aa5c27382ba1d310d
Nikko <dumriquenikko@gmail.com>
2022-01-15 18:57:04 -0800

    Added grid view of posts

--- mu/src/App.js
+++ mu/src/App.js
@@ -1,19 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 import Title from './Header.js';
 import ImageGrid from './ImageGrid';
 import Post from './Post';
+import React, {useState, useEffect} from 'react';
+import Grid from '@material-ui/core/Grid';
 
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "test_username_1",
+     caption:"test_caption_1",
+     nft_name: "A Bored Ape_1",
+     img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+    },
+     {
+       username: "test_username_2",
+     caption:"test_caption_2",
+     nft_name: "A Bored Ape_2",
+     img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+    },
+     {
+       username: "test_username_3",
+     caption:"test_caption_3",
+     nft_name: "A Bored Ape_3",
+     img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+    },
+    {
+      username: "test_username_3",
+    caption:"test_caption_3",
+    nft_name: "A Bored Ape_3",
+    img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+   },
+   {
+    username: "test_username_3",
+    caption:"test_caption_3",
+    nft_name: "A Bored Ape_3",
+    img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+    },
+    {
+      username: "test_username_3",
+    caption:"test_caption_3",
+    nft_name: "A Bored Ape_3",
+    img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+    },
+    {
+      username: "test_username_3",
+    caption:"test_caption_3",
+    nft_name: "A Bored Ape_3",
+    img_url:"https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"
+    }
+  ]);
+
+  // Pull data from data
+  useEffect(() => {
+  // !!! TODO Pull data
+  }, []);
   return (
     <div className="app">
       <div className="app__header"> <Title /> </div>
-      <ImageGrid/>
-      <Post username="Emma Chu" caption="JavaScript > TypeScript" nft_name="lolol1" img_url = "https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"/>
-      <Post username="Michael Ge" caption="COVALENT WOOT WOOT" nft_name="lolasdasd2" img_url = "https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"/>
-      <Post username="Nikko Dumrique" caption="LOL REACT <3" nft_name="lolteeee3" img_url = "https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"/>
-      <Post username="1" caption="test" img_url = "https://pbs.twimg.com/profile_images/1430659995419353092/3RtpXJQH_400x400.jpg"/>
+      {/* <ImageGrid/> */}
+      <Grid container className="grid__container" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+      {
+          posts.map(post => (
+            <Post username={post.username} caption={post.caption} 
+             nft_name={post.nft_name} img_url={post.img_url}></Post> 
+          ))
+
+        }
+      </Grid>
       {/* Posts */}
     </div>
   );
